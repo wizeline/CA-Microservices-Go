246c40336c7b547ba0efcf67156f9e4eb4107f9c
Marcos Jauregui <marcos.jauregui@wizeline.com>
2024-03-08 17:09:58 -0600

    feat: added zerolog support

--- go.mod
+++ go.mod
@@ -5,8 +5,15 @@ go 1.22.0
 require github.com/lib/pq v1.10.9
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.32.0
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/infrastructure/logger/zerolog.go
+++ internal/infrastructure/logger/zerolog.go
@@ -1,1 +1,36 @@
 package logger
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+// Note: add time format types supported by zerolog as needed
+const (
+	DefaultTimeFormat TimeFormat = "2006/01/02 15:04:05"
+)
+
+type ZeroLog struct {
+	logger *zerolog.Logger
+}
+
+type TimeFormat string
+
+func (tf TimeFormat) String() string {
+	return string(tf)
+}
+
+func NewZeroLog(timeFormat TimeFormat) *ZeroLog {
+	writer := zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
+		w.Out = os.Stderr
+		w.TimeFormat = timeFormat.String()
+	})
+
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	zl := zerolog.New(writer).With().Timestamp().Logger()
+
+	return &ZeroLog{
+		logger: &zl,
+	}
+}
