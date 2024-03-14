a12ddddccb1d38fc5bb0d94942517c546cece333
Marcos Jauregui <marcos.jauregui@wizeline.com>
2024-03-14 00:25:25 -0600

    chore: added extra user fields

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -3,13 +3,16 @@ package entity
 import "time"
 
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Password string
-	Email    string
-	BirthDay time.Time
-	Active   bool
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	BirthDay  time.Time
+
+	Username  string
+	Passwd    string
+	Active    bool
+	LastLogin time.Time
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
