cdc755e7855f642cbdc261d77c0683813a54702c
Maritza <maritzaesparza05@gmail.com>
2024-03-18 01:09:09 -0600

    feat: implement user service (#3)

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/wizeline/CA-Microservices-Go/internal/domain/entity"
 	repo "github.com/wizeline/CA-Microservices-Go/internal/domain/repository"
 	svc "github.com/wizeline/CA-Microservices-Go/internal/domain/service"
@@ -20,13 +22,19 @@ func NewUser(repo repo.UserRepository) UserService {
 }
 
 func (s UserService) Add(user entity.User) (int, error) {
-	// TODO: Implement me!
-	return 0, nil
+	err := s.repo.Create(user)
+	if err != nil {
+		return -1, fmt.Errorf("Couldn't create user: %w", err)
+	}
+	return user.ID, nil
 }
 
 func (s UserService) Get(id int) (entity.User, error) {
-	// TODO: Implement me!
-	return entity.User{}, nil
+	user, err := s.repo.Read(id)
+	if err != nil {
+		return entity.User{}, fmt.Errorf("Couldn't get user: %w", err)
+	}
+	return user, nil
 }
 
 func (s UserService) Find(filter, value string) ([]entity.User, error) {
@@ -35,12 +43,18 @@ func (s UserService) Find(filter, value string) ([]entity.User, error) {
 }
 
 func (s UserService) Update(id int, data entity.User) error {
-	// TODO: Implement me!
+	err := s.repo.Update(data)
+	if err != nil {
+		return fmt.Errorf("Couldn't update user: %w", err)
+	}
 	return nil
 }
 
 func (s UserService) Delete(id int) error {
-	// TODO: Implement me!
+	err := s.repo.Delete(id)
+	if err != nil {
+		return fmt.Errorf("Couldn't delete user: %w", err)
+	}
 	return nil
 }
 
@@ -55,6 +69,9 @@ func (s UserService) ChangeEmail(id int, email string) error {
 }
 
 func (s UserService) IsActive(id int) (bool, error) {
-	// TODO: Implement me!
-	return false, nil
+	user, err := s.repo.Read(id)
+	if err != nil {
+		return false, fmt.Errorf("Couldn't get user: %w", err)
+	}
+	return user.Active, nil
 }
